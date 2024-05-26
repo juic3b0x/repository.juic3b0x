7ab323df63f61ce115bf93abdfb8a4c06eeafc77
juic3b0x <70825623+juic3b0x@users.noreply.github.com>
2024-05-26 08:17:59 +0000

    feat: made more secure

--- repo/context.wutu/contextSettings.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-import xbmc
-
-if __name__ == '__main__':
-	plugin = 'plugin://plugin.video.wutu/'
-	path = 'RunPlugin(%s?action=tools_contextWutuSettings&opensettings=false)' % plugin
-	xbmc.executebuiltin(path)
-	# xbmc.executebuiltin('RunPlugin(%s?action=widgetRefresh)' % plugin) #now part of "tools_contextWutuSettings" action
--- repo/plugin.video.wutu/resources/artwork/genre_media/icons/shadow.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-counter=0
-until [ $counter -gt 250 ]
-do
-echo -n "Filename : "
-read filename
-convert "${filename}.png" \( +clone -background black -shadow 50x10+15+15 \) +swap -background none -layers merge +repage "${filename}.png"
- ((counter++))
-done
